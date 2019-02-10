2094ba73d24ae92ce155ea0eb28d7d6fc13f9410
Michal Moskal <michal@moskal.me>
2019-02-09 21:34:18 -0800

    Config fixes for 401c

--- boards/f401c/board.h
+++ boards/f401c/board.h
@@ -17,8 +17,8 @@ const uint32_t configData[] = {
     1, 0x2d, // PIN_ACCELEROMETER_INT = PC13
     2, 0x1a, // PIN_ACCELEROMETER_SCL = PIN_D6
     3, 0x13, // PIN_ACCELEROMETER_SDA = PIN_D7
-    4, 0x2e, // PIN_BTN_A = PC14
-    5, 0x2f, // PIN_BTN_B = PC15
+    4, 0x18, // PIN_BTN_A = PB08
+    5, 0xf, // PIN_BTN_B = PA15
     13, 0x10, // PIN_LED = PB00
     18, 0x1e, // PIN_MISO = PB14
     19, 0x1f, // PIN_MOSI = PB15
@@ -40,7 +40,7 @@ const uint32_t configData[] = {
     43, 0x17, // PIN_DISPLAY_RST = PB07
     44, 0x19, // PIN_DISPLAY_BL = PB09
     47, 0x4, // PIN_BTN_LEFT = PA04
-    48, 0x29, // PIN_BTN_RIGHT = PC09
+    48, 0xa, // PIN_BTN_RIGHT = PA10
     49, 0x15, // PIN_BTN_UP = PB05
     50, 0x12, // PIN_BTN_DOWN = PB02
     51, 0x1, // PIN_BTN_MENU = PA01
@@ -52,8 +52,8 @@ const uint32_t configData[] = {
     66, 0x10, // PIN_JACK_BUSLED = PB00
     67, 0x11, // PIN_JACK_COMMLED = PB01
     70, 0x38, // ACCELEROMETER_TYPE = MMA8453
-    76, 0x18, // PIN_VIBRATION = PB08
-    77, 0xf, // PIN_PWREN = PA15
+    76, 0x2e, // PIN_VIBRATION = PC14
+    77, 0x2f, // PIN_PWREN = PC15
     151, 0x2, // PIN_D1 = PA02
     152, 0x3, // PIN_D2 = PA03
     153, 0x1f, // PIN_D3 = PB15
@@ -63,7 +63,7 @@ const uint32_t configData[] = {
     157, 0x13, // PIN_D7 = PB03
     204, 0x80000, // FLASH_BYTES = 0x80000
     205, 0x18000, // RAM_BYTES = 0x18000
-    208, 0x59ea3b60, // BOOTLOADER_BOARD_ID = 0x449a7cac
+    208, 0x59ea3b60, // BOOTLOADER_BOARD_ID = 0x59ea3b60
     209, 0x57755a57, // UF2_FAMILY = STM32F401
     210, 0x10, // PINS_PORT_SIZE = PA_16
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -74,4 +74,3 @@ const uint32_t configData[] = {
 #endif
 
 #endif /* BOARD_H */
-
