04794f57df8d5042d35ebe927a3337c5438e36b5
Michal Moskal <michal@moskal.me>
2019-02-25 14:29:34 -0800

    Update config for patch-v0 (v1)

--- boards/patch-v0/board.h
+++ boards/patch-v0/board.h
@@ -14,10 +14,10 @@ __attribute__((section(".config"))) __attribute__((used)) //
 const uint32_t configData[] = {
     /* CF2 START */
     513675505, 539130489, // magic
-    48, 100,  // used entries, total entries
+    50, 100,  // used entries, total entries
     1, 0x2d, // PIN_ACCELEROMETER_INT = PC13
-    2, 0x1a, // PIN_ACCELEROMETER_SCL = PIN_D6
-    3, 0x13, // PIN_ACCELEROMETER_SDA = PIN_D7
+    2, 0x1a, // PIN_ACCELEROMETER_SCL = PIN_SCL
+    3, 0x13, // PIN_ACCELEROMETER_SDA = PIN_SDA
     4, 0x18, // PIN_BTN_A = PB08
     5, 0xf, // PIN_BTN_B = PA15
     18, 0x1e, // PIN_MISO = PB14
@@ -26,7 +26,7 @@ const uint32_t configData[] = {
     23, 0x1d, // PIN_SCK = PB13
     24, 0x1a, // PIN_SCL = PB10
     25, 0x13, // PIN_SDA = PB03
-    26, 0x11, // PIN_SPEAKER_AMP = PB01
+    26, 0x10, // PIN_SPEAKER_AMP = PB00
     28, 0x2, // PIN_TX = PA02
     32, 0x5, // PIN_DISPLAY_SCK = PA05
     33, 0x6, // PIN_DISPLAY_MISO = PA06
@@ -45,11 +45,12 @@ const uint32_t configData[] = {
     49, 0x15, // PIN_BTN_UP = PB05
     50, 0x12, // PIN_BTN_DOWN = PB02
     51, 0x1, // PIN_BTN_MENU = PA01
+    59, 0x0, // SPEAKER_VOLUME = 0
     60, 0x16, // PIN_JACK_TX = PB06
     64, 0x0, // PIN_JACK_PWREN = PA00
     65, 0x8, // PIN_JACK_SND = PA08
     66, 0x10, // PIN_JACK_BUSLED = PB00
-    70, 0x38, // ACCELEROMETER_TYPE = MMA8453
+    70, 0x32, // ACCELEROMETER_TYPE = LIS3DH
     151, 0x2, // PIN_D1 = PA02
     152, 0x3, // PIN_D2 = PA03
     153, 0x1f, // PIN_D3 = PB15
@@ -62,16 +63,18 @@ const uint32_t configData[] = {
     208, 0x59ea3b60, // BOOTLOADER_BOARD_ID = 0x59ea3b60
     209, 0x57755a57, // UF2_FAMILY = STM32F401
     210, 0x10, // PINS_PORT_SIZE = PA_16
-    212, 0x40400c0, // _212 = 0x40400c0
+    211, 0x0, // BOOTLOADER_PROTECTION = 0
+    212, 0x78, // POWER_DEEPSLEEP_TIMEOUT = 120
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0,
     /* CF2 END */
 };
 #endif
 
 #endif /* BOARD_H */
+
 
 
 
