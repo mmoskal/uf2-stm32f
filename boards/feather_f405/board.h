584b791316af78430b74f3062827a88687fd2994
ladyada <limor@ladyada.net>
2019-10-13 00:59:40 -0400

    a generic feather stm32f405 bootloader. not fully tested, but def enumerates right

---
+++ boards/feather_f405/board.h
@@ -0,0 +1,32 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#define OSC_FREQ 12
+#define USBDEVICESTRING "Feather F405"
+#define USBMFGSTRING "Adafruit"
+#define BOARD_FLASH_SECTORS 8
+#define BOARD_FLASH_SIZE (512 * 1024)
+
+#ifdef DEFINE_CONFIGDATA
+__attribute__((section(".config"))) __attribute__((used)) //
+const uint32_t configData[] = {
+    /* CF2 START */
+    513675505, 539130489, // magic
+    27, 100,  // used entries, total entries
+    204, 0x80000, // FLASH_BYTES = 0x80000
+    205, 0x20000, // RAM_BYTES = 0x20000
+    208, 0x69d51bac, // BOOTLOADER_BOARD_ID = 0x69d51bac
+    209, 0x57755a57, // UF2_FAMILY = STM32F401
+    210, 0x10, // PINS_PORT_SIZE = PA_16
+    211, 0x0, // BOOTLOADER_PROTECTION = 0
+    212, 0x78, // POWER_DEEPSLEEP_TIMEOUT = 120
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    /* CF2 END */
+};
+#endif
+
+#endif /* BOARD_H */
