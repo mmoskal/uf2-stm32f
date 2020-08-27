d10d84d3908c9580e12999d48ad8162049758c1c
Michal Moskal <michal@moskal.me>
2020-08-27 16:45:56 +0200

    Allow writing settings area from current.uf2 files

--- uf2cfg.h
+++ uf2cfg.h
@@ -5,7 +5,7 @@
 #define INDEX_URL "https://arcade.makecode.com"
 #define UF2_NUM_BLOCKS 8000
 #define VOLUME_LABEL "ARCADE-F4"
-// where the UF2 files are allowed to write data - we allow MBR, since it seems part of the softdevice .hex file
-#define USER_FLASH_START (uint32_t)(APP_LOAD_ADDRESS)
+// where the UF2 files are allowed to write data; we let them write to settings area as well
+#define USER_FLASH_START (uint32_t)(APP_LOAD_ADDRESS - 32 * 1024)
 #define USER_FLASH_END (0x08000000+BOARD_FLASH_SIZE)
 
